e0c8743e281adb8c4153ca5e4c07786874e8ca07
yxynicole <ye.xinyu@northeastern.edu>
2022-02-20 22:12:28 -0500

    heroku

--- server.ts
+++ server.ts
@@ -10,13 +10,15 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.get('/', (req: any, res: { send: (arg0: string) => any; }) => res.send('Welcome!'))
+
 new UserController(app);
 new TuitController(app);
 
-mongoose.connect('mongodb://0.0.0.0:27017/tuiter')
+//mongoose.connect('mongodb://0.0.0.0:27017/tuiter')
+mongoose.connect('mongodb+srv://admin:'+process.env.MONGO_PW+'@cluster0.bihkn.mongodb.net/tuiter?retryWrites=true&w=majority')
 mongoose.connection.on('connected', () => console.log("connected"))
 
-app.listen(4000, () => {
+app.listen(process.env.PORT || 4000, () => {
     console.log("listen to port", 4000)
 });
-
